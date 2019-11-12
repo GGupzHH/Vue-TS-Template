591979b3590de36d6494a1a98421f6159c596323
GGupzhh <1248023357@qq.com>
2019-11-12 13:40:50 +0800

    Add Axios http

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -6,12 +6,12 @@ axios.defaults.baseURL = process.env.VUE_APP_BASEURL ? process.env.VUE_APP_BASEU
 
 // headers Content-type
 axios.defaults.headers = {
-  "Content-Type": 'application/json;charset=utf8'
+  'Content-Type': 'application/json;charset=utf8'
 }
 
 // 请求 拦截器
 axios.interceptors.request.use(
-  config => {
+  (config: any) => {
     return config
   }, (error: any) => {
     Promise.reject(error)
@@ -20,7 +20,7 @@ axios.interceptors.request.use(
 
 // 响应 拦截器
 axios.interceptors.response.use(
-  response => {
+  (response: any) => {
     if (response.status === 200) {
       if (response.data.code !== 0) {
         return response.data
@@ -40,7 +40,49 @@ axios.interceptors.response.use(
       })
       return Promise.reject(new Error('Serve Error'))
     }
-  }, error => {
+  }, (error: any) => {
     Promise.reject(error)
   }
 )
+
+function formateURLOnlyGet(link: string, json: object) {
+  let url = link
+  const data = Object.entries(json)
+  if (data.length) {
+    url += url.indexOf('?') == -1 ? '?' : ''
+    url += Object.entries(data)
+      .map((item: any) => {
+        return item[1].join('=')
+      })
+      .join('&')
+  }
+  return url
+}
+//  get url
+export function httpGet(url: string, data: object) {
+  return axios
+    .get(formateURLOnlyGet(url, data))
+    .then((res: any) => res.data)
+    .catch((error: any) => error.response)
+}
+// post
+export function httpPost(url: string, data: object) {
+  return axios
+    .post(url, data)
+    .then((res: any) => res.data)
+    .catch((error: any) => error.response)
+}
+// put
+export function httpPut(url: string, data: object) {
+  return axios
+    .put(url, data)
+    .then((res: any) => res.data)
+    .catch((error: any) => error.response)
+}
+// delete
+export function httpDelete(url: string, data: object) {
+  return axios
+    .delete(url, data)
+    .then((res: any) => res.data)
+    .catch((error: any) => error.response)
+}
